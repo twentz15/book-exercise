9f3780c3fc70df5829671b42998e13a964766952
Tom Wentz <twentz15@outlook.com>
2016-02-08 21:39:18 -0500

    Exercise 2.83

--- Book.java
+++ Book.java
@@ -21,6 +21,18 @@ class Book
         author = bookAuthor;
         title = bookTitle;
     }
+    
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    
 
     // Add the methods here ...
 }
